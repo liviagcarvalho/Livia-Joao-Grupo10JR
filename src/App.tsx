fd714dd97a6eac46d2dae1102d58371cca0463f0
Lívia Carvalho <liviagc@al.insper.edu.br>
2025-04-16 20:31:26 -0300

    pagprod

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import Header from './components/Header';
-import HeroSection from "./components/HeroSection";
-import ProductB2B from './pages/ProductB2B';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import HomeB2B from './pages/HomeB2B';
+import ProductB2B from './pages/ProductB2B';
 
 function App() {
   return (
     <Router>
       <Header />
-      <HeroSection />
       <Routes>
+        <Route path="/" element={<HomeB2B />} />
         <Route path="/produtos" element={<ProductB2B />} />
         <Route path="/produtos/:categoria" element={<ProductB2B />} />
       </Routes>
@@ -18,5 +18,3 @@ function App() {
 }
 
 export default App;
-
-
