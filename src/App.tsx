85aa36f21b982131df984c0988f63b5722db935c
joao <joaoham@al.insper.edu.br>
2025-04-20 19:48:26 -0300

    homeb2c esteticamente, carrinho 100 funcional

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,23 @@ import HomeB2B from './pages/HomeB2B';
 import ProductB2B from './pages/ProductB2B';
 import AjudaB2B from './pages/AjudaB2B';
 import SobreB2B from './pages/SobreB2B';
-import HomeB2C from './pages/HomeB2C'
+import HomeB2C from './pages/HomeB2C';
+import { CartProvider } from './components/CartContext'; // 👈 importa o provider
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/b2c" element={<HomeB2C />} />
-        <Route path="/ajuda-b2b" element={<AjudaB2B />} />
-        <Route path="/sobre" element={<SobreB2B />} />
-        <Route path="/" element={<HomeB2B />} />
-        <Route path="/produtos" element={<ProductB2B />} />
-        <Route path="/produtos/:categoria" element={<ProductB2B />} />
-      </Routes>
-    </Router>
+    <CartProvider> 
+      <Router>
+        <Routes>
+          <Route path="/b2c" element={<HomeB2C />} />
+          <Route path="/ajuda-b2b" element={<AjudaB2B />} />
+          <Route path="/sobre" element={<SobreB2B />} />
+          <Route path="/" element={<HomeB2B />} />
+          <Route path="/produtos" element={<ProductB2B />} />
+          <Route path="/produtos/:categoria" element={<ProductB2B />} />
+        </Routes>
+      </Router>
+    </CartProvider>
   );
 }
 
