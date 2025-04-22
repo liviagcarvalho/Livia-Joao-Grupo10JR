f7238bed64ebca28c4ce9e2ffe017e1a2bfbac62
joao <joaoham@al.insper.edu.br>
2025-04-21 21:46:50 -0300

    Tudo  q eu falei no wpp dia 21/4 as 21:46

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import Header from './components/Header';
 import HomeB2B from './pages/HomeB2B';
 import ProductB2B from './pages/ProductB2B';
 import AjudaB2B from './pages/AjudaB2B';
+import AjudaB2C from './pages/AjudaB2C';
 import SobreB2B from './pages/SobreB2B';
+import SobreB2C from './pages/SobreB2C';
 import HomeB2C from './pages/HomeB2C';
 import { CartProvider } from './components/CartContext'; // 👈 importa o provider
 
@@ -15,6 +17,8 @@ function App() {
         <Routes>
           <Route path="/b2c" element={<HomeB2C />} />
           <Route path="/ajuda-b2b" element={<AjudaB2B />} />
+          <Route path="/ajuda-b2c" element={<AjudaB2C />} />
+          <Route path="/sobre-b2c" element={<SobreB2C />} />
           <Route path="/sobre" element={<SobreB2B />} />
           <Route path="/" element={<HomeB2B />} />
           <Route path="/produtos" element={<ProductB2B />} />
